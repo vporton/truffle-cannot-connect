cc7ce2e2de571ae57f9eafdab5a395afb0f536d1
Victor Porton <porton@narod.ru>
2020-12-28 05:27:56 +0200

    simplified

--- migrations/02_deploy_climate.js
+++
@@ -1,7 +0,0 @@
-const { myDeploy } = require('../lib/default-deployer');
-
-module.exports = async function(deployer, network, accounts) {
-  const uuid = '1bb89f0c-47ab-11eb-9b52-2bc780f5fac3';
-  const climate = await myDeploy(deployer, network, accounts, "Lock", `urn:uuid:${uuid}`);
-  // await climate.createOracle({contractAddress: carbon, tokenId: retiredCarbonTokenId})
-};
