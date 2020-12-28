68dd22ea0e9a98e5f8e8fa308902d61b4c0b9871
Victor Porton <porton@narod.ru>
2020-12-28 05:37:38 +0200

    code formatting

--- migrations/03_deploy_science.js
+++ migrations/03_deploy_science.js
@@ -2,7 +2,5 @@ module.exports = async function(deployer, network, accounts) {
   const contract = artifacts.require("BaseLock");
   await deployer.deploy(contract, {overwrite: false, from: accounts[0]}); // `from` for Truffle 5.1.58 bug
   const science = await contract.deployed();
-
-  // const science = await myDeploy(deployer, network, accounts, "BaseLock");
   await science.createOracle({ from: deployer });
 };
