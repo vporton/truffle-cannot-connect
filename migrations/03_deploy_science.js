b09b63b19fe1bfddbfd357ca97fa193074f112f2
Victor Porton <porton@narod.ru>
2020-12-28 05:33:59 +0200

    simplified

--- migrations/03_deploy_science.js
+++ migrations/03_deploy_science.js
@@ -5,7 +5,6 @@ module.exports = async function(deployer, network, accounts) {
   const uuid = '4a4552a6-4644-11eb-a830-3f3c92c66629';
   const DefaultDAOInterface = artifacts.require("DefaultDAOInterface");
   const dao = deployer; // FIXME: Add owner DAO.
-  await deployProxy(DefaultDAOInterface, [], { deployer: dao });
   const science = await myDeploy(deployer, network, accounts, "BaseLock");
   await science.createOracle({ from: dao });
 };
