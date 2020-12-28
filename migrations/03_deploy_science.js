e30b57298c718c0f66d149f0b4d5ef67c1f3e2cb
Victor Porton <porton@narod.ru>
2020-12-28 05:30:50 +0200

    simplified

--- migrations/03_deploy_science.js
+++ migrations/03_deploy_science.js
@@ -7,23 +7,5 @@ module.exports = async function(deployer, network, accounts) {
   const dao = deployer; // FIXME: Add owner DAO.
   await deployProxy(DefaultDAOInterface, [], { deployer: dao });
   const science = await myDeploy(deployer, network, accounts, "BaseLock", `urn:uuid:${uuid}`);
-  ({ logs } = await science.createOracle({ from: dao }));
-  const oracleId = logs[0].args.oracleId;
-
-  // TODO: duplicate code
-  {
-    const addressesFileName = `frontend/public/addresses.json`;
-    let json;
-    try {
-        const text = fs.readFileSync(addressesFileName);
-        json = JSON.parse(text);
-    }
-    catch(_) {
-        json = {};  
-    }
-    updateAddress(json, network, 'data', {
-        scienceOracleId: oracleId,
-    });
-    fs.writeFileSync(addressesFileName, JSON.stringify(json));
-  }
+  await science.createOracle({ from: dao });
 };
